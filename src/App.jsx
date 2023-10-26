53ae2f9eedcfe6a463c2e2a7524daa8b9c7a6662
brillbray <brillbrizz128@gmail.com>
2023-10-26 15:49:34 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Header from  './components/header/Header'
-import Navbar from './components/navbar/Navbar'
-import About from './components/aboutMe/About'
-import Experience from './components/experience/Experience'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contactMe/Contact'
-import Footer from './components/footer/Footer'
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import Header from  './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import About from './components/aboutMe/About';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contactMe/Contact';
+import Footer from './components/footer/Footer';
+import reportWebVitals from "./reportWebVitals";
 
-const App = () => {
-  return (
-    <>
-        <Header />
-        <Navbar />
-        <About />
-        <Experience />
-        <Portfolio />
-        <Contact />
-        <Footer /> 
-    </>
-  )
-}
+
+const App = ReactDOM.createRoot(document.getElementById("App"));
+App.render(
+  <React.StrictMode>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer /> 
+  </React.StrictMode>
+);
+
+reportWebVitals();
 
 export default App
